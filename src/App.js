2f8d68e8b1d625acf47a1fa0b76602a611ecaa6b
Luis Mex <lmex89@lmex89.ipv6.infinitum.net.mx>
2022-08-21 18:17:37 -0500

    commit last changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import axios from "axios";
+import React from "react";
+
+const baseURL = "http://192.168.1.200:8000/api/v1/temp_list/?view=code&code=core_0";
+
+
+function getingTempdata(data) {
+  let result = []
+  for (let key in data){
+    result.push(data[key].temp.value)
+  }
+  return result;
+}
+
+export default function App() {
+  const [dataRequest, setDatarequest] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setDatarequest(response.data);
+    });
+  }, []);
+
+  if (!dataRequest) return null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2> Current Cpu temp:  {dataRequest.at(-1).temp.value} °C</h2>
+      <h2> Current Mem Ram Used : {dataRequest.at(-1).ram.value_used} MB </h2>
+      <h2> Current Mem Ram Available : {dataRequest.at(-1).ram.value_available} MB </h2>
+      <h2> Current Mem Ram Total : {dataRequest.at(-1).ram.value_total} MB </h2>
     </div>
   );
 }
 
-export default App;
